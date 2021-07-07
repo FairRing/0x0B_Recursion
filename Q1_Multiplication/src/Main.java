f52d9522b353e117e2bd8bcd7310d72608ca7990
FairRing <kjh122333@gmail.com>
2021-07-07 23:45:44 +0900

    tlqkf

---
+++ Q1_Multiplication/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input[] = reader.readLine().split(" ");
+        long a = Integer.parseInt(input[0]);
+		long b = Integer.parseInt(input[1]);
+		long c = Integer.parseInt(input[2]);
+
+        System.out.println(power(a % c, b, c));
+    }
+
+    public static long power(long a, long b, long c) {
+        
+        if(b == 0) return 1; // b == 0
+        if(b == 1) return a; // b == 1 
+                
+        long temp = power(a, b/2, c) % c;
+        
+        if(b % 2 == 0) return (temp * temp) % c; // b == 짝수
+        else return (((temp * temp) % c) * a) % c; //  b == 홀수
+    }
+}
