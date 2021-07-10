a46bb1b6b59baadbf8a42637d6cc22b681e94011
FairRing <kjh122333@gmail.com>
2021-07-10 22:39:36 +0900

    done.

--- Q2_HanoiTower/src/Main.java
+++ Q2_HanoiTower/src/Main.java
@@ -1,26 +1,30 @@
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Stack;
-
 public class Main {
-    static int N, K;
-    static Stack<Integer> t1, t2, t3;
-    static ArrayList<int[]> history = new ArrayList<>();
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        N = scanner.nextInt();
-        K = 0;
-        t1 = new Stack<>();
-        t2 = new Stack<>();
-        t3 = new Stack<>();
-        for (int i = N; i > 0; i--) t1.push(i);
 
-        // 한 번에 한 개의 원판만을 다른 탑으로 옮길 수 있다.
-        // 쌓아 놓은 원판은 항상 위의 것이 아래의 것보다 작아야 한다.
-        
+	static StringBuilder builder = new StringBuilder();
+	static int count = 0;
 
+	public static void hanoi(int n, int start, int goal, int temp) {
+		if (n == 0) {
+			return;
+		} else {
+			count++;
+			hanoi(n - 1, start, temp, goal);
+			builder.append(start + " " + goal + "\n");
+			hanoi(n - 1, temp, goal, start);
+		}
+	}
 
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+
+		hanoi(n, 1, 3, 2);
+
+		System.out.println(count);
+		System.out.println(builder);
 
         scanner.close();
-    }
+	}
+
 }
